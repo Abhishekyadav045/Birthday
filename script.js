80603611a578498d5093f67098d7f861ed54f1e5
Abhishek Yadav <ay407473@gmail.com>
2026-02-05 13:27:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showSurprise() {
+  document.getElementById("surprise").innerText =
+    "Again wish you very happy birthday my dear cutie thanks for always supports me and motivate me ..i hope you always stay with me  🎉 Wishing you a year full of happiness, success & beautiful moments 💖";
+}
